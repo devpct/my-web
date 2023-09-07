c9ce3ae9ca8aeb9db8a124275845fb59f0e5d16f
devpct <mr.mohammad.pg19@gmail.com>
2023-09-07 12:30:59 +0330

    start project

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>Home - DevPCT</title>
+    </Head>
+    <h1 className="">mmd</h1>
+    </>
+  )
+}
