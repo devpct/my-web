187472038906c9a028b3422bb48d915e8367ee8b
jcode2023 <mr.mohammad.pg19@gmail.com>
2023-12-02 21:34:23 +0330

    debug

--- components/home/socialNetworkBox.tsx
+++ components/home/socialNetworkBox.tsx
@@ -1,29 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 interface SocialNetworkBoxProps {
-    backgroundColor: string;
-    images: string[];
-    colorText: string[];
-    title: string[];
-    FST: string[];
-  }
+  backgroundColor: string;
+  images: string[];
+  colorText: string[];
+  title: string[];
+  link: string[];
+  FST: string[];
+}
 
-function socialNetworkBox({ backgroundColor, images, colorText, title, FST }: SocialNetworkBoxProps) {
+function SocialNetworkBox({ backgroundColor, images, colorText, title, link, FST }: SocialNetworkBoxProps) {
   return (
     <div className="flex flex-wrap gap-x-[9rem] gap-y-[2rem] w-full h-full justify-center">
       {images.map((img, index) => (
-          <div
+        <a
           key={index}
           className={`flex items-center ${images.length === 1 ? 'lg:w-[88%]' : 'lg:w-[40%]'} w-full lg:h-[142px] h-[100px] rounded-[28px] shadow-2xl cursor-pointer justify-center ${title[index] === '' ? 'gap-x-0' : 'gap-x-8'}`}
-          style={{backgroundColor: backgroundColor}}
-          >
+          style={{ backgroundColor: backgroundColor }}
+          href={link?.[index]}
+        >
           <img className='lg:w-[5.2rem] w-[4rem]' src={img} loading='lazy' />
-          <p className={`lg:text-[${FST[index]}] text-[2.2rem] font-medium`}
-          style={{color: colorText[index]}}>{title[index]}</p>
-        </div>
+          <p className={`lg:text-[${FST[index]}] text-[2.2rem] font-medium`} style={{ color: colorText[index] }}>{title[index]}</p>
+        </a>
       ))}
     </div>
-  )
+  );
 }
 
-export default socialNetworkBox
+export default SocialNetworkBox;
--- components/home/socialNetworkBoxes.tsx
+++ components/home/socialNetworkBoxes.tsx
@@ -10,6 +10,7 @@ function socialNetworkBoxes() {
         images={['./images/home/gmail-icon.png', './images/home/linkedin.png']}
         colorText={['#33333394', '#0A66C2']}
         title={['Gmail', 'Linkedin']}
+        link={['mailto:mrdevpct@gmail.com', 'https://www.linkedin.com/in/devpct']}
         FST={['3.3rem','3.3rem']}
       />
 
@@ -18,6 +19,7 @@ function socialNetworkBoxes() {
         images={['./images/home/codepen.png', './images/home/stackoverflow.png']}
         colorText={['#fff', '#DA7526']}
         title={['Codepen', 'Stackoverflow']}
+        link={['https://codepen.io/devpct', 'https://stackoverflow.com/users/18831761/mohammad-abdollahzadeh']}
         FST={['3.3rem','2.5rem']}
       />
 
@@ -26,6 +28,7 @@ function socialNetworkBoxes() {
         images={['./images/home/x.png', './images/home/instagram.png']}
         colorText={['', '#33333394']}
         title={['', 'Instagram']}
+        link={['https://www.twitter.com/devpct', 'https://www.instagram.com/devpct']}
         FST={['3.3rem','3.3rem']}
       />
 
@@ -34,6 +37,7 @@ function socialNetworkBoxes() {
         images={['./images/home/github.png']}
         colorText={['#33333394']}
         title={['Github']}
+        link={['https://github.com/devpct']}
         FST={['3.3rem','3.3rem']}
       />
 
